e80de9a5a087afcfcd8a66dc0318563b0f90c945
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-02-24 00:22:12 +0800

    chore: tsup init

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  dts: true,
+  clean: true,
+  minify: true,
+  treeshake: true,
+  splitting: true,
+  entry: ['./src/index.ts'],
+  format: ['cjs', 'esm'],
+});
